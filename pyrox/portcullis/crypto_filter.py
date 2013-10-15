b2b877fc9491938d8526bc0cf09e271bbb8e0155
Paul Kehrer <paul.l.kehrer@gmail.com>
2013-10-15 12:29:26 -0500

    some cleanup and reorganization (more required). hmac support
    This stuff doesn't work right now due to the following issues:
    * pyrox is not generating new filter objects per request which causes
      some state mixing
    * pyrox is not sending an empty msg_part when a response is complete so
      we don't know when a response ends to do the hmac comparison
    * data is being stored in a buffer internally rather than throttling the
      request/response

--- pyrox/portcullis/crypto_filter.py
+++ pyrox/portcullis/crypto_filter.py
@@ -1,30 +1,8 @@
 import pyrox.filtering as filtering
 
 from Crypto.Cipher import AES
-
-
-def process_chunk(msg_part, processor, output):
-    if msg_part:
-        # self.buffer_mgr.receive_data(msg_part)
-        # output_part = self.buffer_mgr.read_all_modulo()
-        output_part = processor.process_data(msg_part)
-        #output.write(output_part)
-        if output_part:
-            print("!!!!! Output chunk...message length {}".format(len(msg_part)))  # .format(output_part))
-            output.write(output_part)
-        else:
-            print("!!!!! Not enough data for message length {}".format(len(msg_part)))
-            # if True:
-            #     raise Exception("uuuuuuuu")
-            output.write("")
-    else:
-        # output_part = self.buffer_mgr.read_all()
-        output_part = processor.finish()
-        print("!!!!! Final: {}".format(output_part))
-        output.write(output_part)
-
-    # if True:
-    #     raise Exception("lkjflksdjflsdjfldsjkf")
+from Crypto.Hash import HMAC, SHA256
+import requests
 
 
 class CryptoFilter(filtering.HttpFilter):
@@ -32,23 +10,35 @@ class CryptoFilter(filtering.HttpFilter):
     This filter encrypts/decrypts data streamed through it on its way
     to/from a Swift encrypted volume.
     """
+    REQUEST = 0
+    RESPONSE = 1
+
     def __init__(self):
         super(CryptoFilter, self).__init__()
+        print("initing new filter")
 
         #TODO(jwood) Just need one of these processors.
         self.processor_upload = SampleCryptoProcessor(is_encrypt=True)
         self.processor_download = SampleCryptoProcessor(is_encrypt=False)
         self.request_method = None
+        self.hmac = HMAC.new("secretkey", digestmod=SHA256.new())
 
     @filtering.handles_request_head
     def on_request_head(self, request_head):
         print(">>>>>>> {}".format(self))
         print('Got request head with verb: {}'.format(request_head.method))
         self.request_method = request_head.method
+        self.url = request_head.url
+        self.auth_token = request_head.header("X-Auth-Token").values[0]
 
     @filtering.handles_response_head
     def on_response_head(self, response_head):
         print('Got response head with status: {}'.format(response_head.status))
+        if response_head.status == '201' and self.request_method == 'PUT':
+            self.set_hmac()
+        hmac_digest = response_head.get_header("X-Object-Meta-Hmac-Digest")
+        if hmac_digest:
+            self.hmac_digest = hmac_digest.values[0]
 
     @filtering.handles_request_body
     def on_request_body(self, msg_part, output):
@@ -71,7 +61,7 @@ class CryptoFilter(filtering.HttpFilter):
             output.write(msg_part)
             return
 
-        process_chunk(msg_part, self.processor_upload, output)
+        self.process_chunk(msg_part, self.REQUEST, output)
 
     @filtering.handles_response_body
     def on_response_body(self, msg_part, output):
@@ -82,23 +72,61 @@ class CryptoFilter(filtering.HttpFilter):
             output.write(msg_part)
             return
 
-        process_chunk(msg_part, self.processor_download, output)
+        self.process_chunk(msg_part, self.RESPONSE, output)
+
+    def process_chunk(self, msg_part, req_resp, output):
+        if msg_part:
+            if req_resp == self.REQUEST:
+                output_part = self.processor_upload.process_data(msg_part)
+                self.hmac.update(output_part)
+            else:
+                self.hmac.update(msg_part)
+                output_part = self.processor_download.process_data(msg_part)
+            if output_part:
+                print("!!!!! Output chunk...message length {}".format(len(msg_part)))  # .format(output_part))
+                output.write(output_part)
+            else:
+                print("!!!!! Not enough data for message length {}".format(len(msg_part)))
+                output.write("")
+        else:
+            if req_resp == self.REQUEST:
+                print('finishing request')
+                output_part = self.processor_upload.finish()
+            else:
+                print('finishing response')
+                output_part = self.processor_download.finish()
+                # this stuff isn't being invoked right now
+                print('done downloading. hmac should be {}'.format(self.hmac_digest))
+                print('done downloading. hmac is {}'.format(self.hmac.hexdigest()))
+            print("!!!!! Final: {}".format(output_part))
+            output.write(output_part)
+
+    def set_hmac(self):
+        print("!!!!! start set hmac")
+        url = "https://storage101.dfw1.clouddrive.com:443" + self.url
+        headers = {"X-Object-Meta-Hmac-Digest": self.hmac.hexdigest(),
+                   "X-Auth-Token": self.auth_token}
+        requests.post(url, headers=headers)
+        print("!!!!! end set hmac")
+
 
 
 #TODO(jwood) Consider adding a base Processor class, that this one extends?
 class SampleCryptoProcessor(object):
     def __init__(self, is_encrypt, block_size_bytes=16):
         self.block_size_bytes = block_size_bytes
+        key = 'sixteen_byte_key'
+        iv = 'sixteen_byte_iv!'
         self.block_method = self._encrypt_block if is_encrypt else self._decrypt_block
-        self.encryptor = AES.new('sixteen_byte_key', AES.MODE_CBC, 'sixteen_byte_iv!')
-        self.decryptor = AES.new('sixteen_byte_key', AES.MODE_CBC, 'sixteen_byte_iv!')
+        self.encryptor = AES.new(key, AES.MODE_CBC, iv)
+        self.decryptor = AES.new(key, AES.MODE_CBC, iv)
         self.last_block = ''
         self.is_encrypt = is_encrypt
 
     def process_data(self, data):
-        """Accept and process the input 'data' block by applying the 'block_method()'
-        to it. Return an 'output' that is a modulo of this processor's block size, which
-        may not be evenly aligned with the input data's size.
+        """Accept and process the input 'data' block. Return an 'output' that
+        is a modulo of this processor's block size, which may not be evenly
+        aligned with the input data's size.
         """
         buff = ''.join([self.last_block, data])
         len_buff = len(buff)
@@ -124,11 +152,10 @@ class SampleCryptoProcessor(object):
         self.last_block = ''
         return output
 
-    #TODO(reaperhulk) Make this secure. ;)
     def _encrypt_block(self, block):
-        return self.encryptor.encrypt(block)
+        block = self.encryptor.encrypt(block)
+        return block
 
-    #TODO(reaperhulk) Make this secure. ;)
     def _decrypt_block(self, block):
         return self.decryptor.decrypt(block)
 
